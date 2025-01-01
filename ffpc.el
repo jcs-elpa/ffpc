55a716d85c6979a46dbf4d241ce3085fdb5ac881
JenChieh <jcs090218@gmail.com>
2025-01-01 02:07:14 -0800

    Update copyright year

--- ffpc.el
+++ ffpc.el
@@ -1,6 +1,6 @@
 ;;; ffpc.el --- Find file in project or current directory  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022-2024  Shen, Jen-Chieh
+;; Copyright (C) 2022-2025  Shen, Jen-Chieh
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
